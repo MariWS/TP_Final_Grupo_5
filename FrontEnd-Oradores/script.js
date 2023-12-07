1d80620594085fb26457230b473f4ff58ef1dd3c
ChristianX2099 <christiananaya2099@gmail.com>
2023-12-07 16:55:49 -0500

    archivos-oradores

---
+++ FrontEnd-Oradores/script.js
@@ -0,0 +1,37 @@
+var capturaFormulario= document.getElementById("caja2")
+console.log(capturaFormulario)
+
+capturaFormulario.addEventListener('submit',function(e){
+    e.preventDefault();
+    ResumenFormulario()
+})
+
+// Calculo
+const valorTicket = 200;
+
+//html elements
+const nombre = document.getElementById("nombrejv");
+const apellido = document.getElementById("apellidojv");
+const correo = document.getElementById("correojv");
+const Cantidad = document.getElementById("cantidadjs").value;
+const Categoria = document.getElementById("categoriajs").value;
+
+function ResumenFormulario() {
+    const Cantidad = document.getElementById("cantidadjs").value;
+    const Categoria = document.getElementById("categoriajs").value;
+  
+    let Descuento = 0; 
+  
+    if (Categoria === "Estudiante") {
+      Descuento = Cantidad * (valorTicket - valorTicket * 0.8);
+    } else if (Categoria === "Trainee") {
+      Descuento = Cantidad * (valorTicket - valorTicket * 0.5);
+    } else if (Categoria === "Junior") {
+      Descuento = Cantidad * (valorTicket - valorTicket * 0.15);
+    }
+    console.log(Descuento);
+    const capturaLabel = document.getElementById("valorInput1");
+
+    capturaLabel.textContent = 'Total a pagar: $' + Descuento;
+  }
+  
