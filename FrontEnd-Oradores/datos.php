f1c3557eb5a2650301a3c584462c18e84223e035
ChristianX2099 <christiananaya2099@gmail.com>
2023-12-09 20:09:02 -0500

    pequenias modificaciones

--- FrontEnd-Oradores/datos.php
+++ FrontEnd-Oradores/datos.php
@@ -1,19 +1,23 @@
  <?php
-  echo"prueba php <br>";
+/*   echo $_GET['nombre'];*/
+  $nombre=$_GET['nombre'];
+  $apellido=$_GET['apellido'];
+  $email=$_GET['email'];
+  $fecha=$_GET['fecha'];
+  $comentarios=$_GET['comentarios'];
 
-/*   echo $_GET['nombre'];
-  echo $_GET['apellido'];
-  echo $_GET['email'];
-  echo $_GET['fecha']; */
- 
   echo "<pre>";
   var_dump($_GET);
   echo "</pre>";
 
- echo "El nombre es: " .$_GET['nombre'];
+  echo "El nombre es: " .$_GET['nombre']."<br>";
 
+  if($nombre=="" && isset($_GET['nombre'])){
+    echo "Esta vacio... ingrese algo";
+  }else{
+    echo"Hay algo en el texto: ". $nombre;
+  }
 
- $nombre=$_GET['nombre'];
 
 ?>
 
@@ -26,12 +30,19 @@
     <title>php y html</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
 
-<h1>Hola esto es html + PHP</h1>
-<h2>hola mi nombres es: <?php echo $nombre  ?></h2>
-<h2>Edad: <?php/*  echo $edad   */?></h2>
+  <h1>Hola esto es HTML + PHP</h1>
+  <h2>Hola mi nombres es: <?php echo $nombre  ?></h2>
 
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $nombre?></h5>
+      <p class="card-text"><?php echo $comentarios?></p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 
 
 <!-- <script>
--- FrontEnd-Oradores/index.php
+++ FrontEnd-Oradores/index.php
@@ -12,7 +12,7 @@ date_default_timezone_set('America/Argentina/Buenos_Aires'); // Ajusta la zona h
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Integrador Anaya Christian</title>
+    <title>Grup 5 integrador</title>
 
 </head>
 
